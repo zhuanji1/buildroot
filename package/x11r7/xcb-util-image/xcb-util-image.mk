e6f19c8568ed771694e8cc7f965f3f322f5c2fe9
pqy <pangqingyuan1991@163.com>
2020-10-20 04:47:18 -0400

    fix

--- package/x11r7/xcb-util-image/xcb-util-image.mk
+++ package/x11r7/xcb-util-image/xcb-util-image.mk
@@ -12,4 +12,10 @@ XCB_UTIL_IMAGE_LICENSE = MIT
 XCB_UTIL_IMAGE_LICENSE_FILES = COPYING
 XCB_UTIL_IMAGE_DEPENDENCIES = xcb-util
 
+define XCB_UTIL_IMAGE_POST_PATCH_FIXUP
+	$(SED) 's%-lxcb-image%-lxcb-image -lxcb-util%' $(STAGING_DIR)/usr/lib/pkgconfig/xcb-image.pc
+endef
+
+XCB_UTIL_IMAGE_POST_INSTALL_STAGING_HOOKS += XCB_UTIL_IMAGE_POST_PATCH_FIXUP
+
 $(eval $(autotools-package))
